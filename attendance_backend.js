c68b4e93f64a312a669727e651e8f4f670eab79e
Muthu-2312103 <2312103@nec.edu.in>
2025-07-25 16:20:47 +0530

    Add backend js form

--- attendance_backend.js
+++ attendance_backend.js
@@ -1,20 +1,17 @@
-const attendanceList = [];
+    const attendanceList = [];
 
-function markAttendance(name) {
-    attendanceList.push(name); 
-    console.log(`${name} is marked present.`);
-}
+    function markAttendance(name) {
+      attendanceList.push(name);
+      console.log(`${name} is marked present.`);
+    }
 
-document.getElementById("attendanceForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    const studentName = this.student.value.trim();
-    
-    if (studentName) {
+    document.getElementById("attendanceForm").addEventListener("submit", function (event) {
+      event.preventDefault();
+      const studentName = this.studentName.value.trim();
+
+      if (studentName) {
         markAttendance(studentName);
         alert(`${studentName} attendance marked!`);
-        this.reset();  
-    } else {
-        alert("Please enter a student name.");
-    }
-});
+        this.reset();
+      }
+    });
